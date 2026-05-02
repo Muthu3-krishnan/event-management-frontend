7ca25bc78722bb8e53a834860c5e4d8b4eff2db6
Muthukrishnan M <muthukrishnan666999@gmail.com>
2026-05-02 13:45:45 +0530

    Update venueService.js

--- src/api/venueService.js
+++ src/api/venueService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://event-management-backend-xm3l.onrender.com/attendee";
+const BASE_URL = "https://event-management-backend-xm3l.onrender.com/venue";
 
 export const getAllVenues = () => {
   return axios.get(BASE_URL);
